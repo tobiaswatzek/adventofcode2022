ed84d86b8184c525b8b7e6148e4cc642a1ef3c74
Tobias Watzek <spitzfaust@gmail.com>
2022-12-02 07:53:05 +0100

    Solution for day2 part 1

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"io/ioutil"
 	"log"
 	"sort"
@@ -11,6 +12,7 @@ import (
 func main() {
 	log.Println("AoC 2022")
 	day1()
+	day2()
 }
 
 func day1() {
@@ -38,6 +40,101 @@ func day1() {
 	log.Println("Day 1")
 	log.Printf("First: %d\n", topThree[0])
 	log.Printf("Second: %d\n", sum(topThree))
+}
+
+func day2() {
+	input := readFile("day2.txt")
+	lines := strings.Split(input, "\n")
+
+	score := 0
+
+	for _, line := range lines {
+		if strings.TrimSpace(line) == "" {
+			continue
+		}
+
+		columns := strings.Split(line, " ")
+		opponentMove, err := parseColumnOne(columns[0])
+		if err != nil {
+			log.Fatal(err)
+		}
+		myMove, err := parseColumnTwo(columns[1])
+		if err != nil {
+			log.Fatal(err)
+		}
+		score += shapeScore(myMove)
+		score += roundScore(opponentMove, myMove)
+	}
+
+	log.Println("Day 2")
+	log.Printf("First: %d\n", score)
+}
+
+type shape int
+
+const (
+	rock shape = iota
+	paper
+	scissors
+)
+
+func roundScore(opponentMove, myMove shape) int {
+	if opponentMove == myMove {
+		return 3
+	}
+
+	if opponentMove == rock && myMove == scissors {
+		return 0
+	}
+
+	if opponentMove == paper && myMove == rock {
+		return 0
+	}
+
+	if opponentMove == scissors && myMove == paper {
+		return 0
+	}
+
+	return 6
+}
+
+func shapeScore(s shape) int {
+	switch s {
+	case rock:
+		return 1
+	case paper:
+		return 2
+	case scissors:
+		return 3
+	default:
+		panic("Shape is not known")
+	}
+}
+
+func parseColumnOne(c string) (shape, error) {
+	switch c {
+	case "A":
+		return rock, nil
+	case "B":
+		return paper, nil
+	case "C":
+		return scissors, nil
+	}
+
+	return rock, errors.New("Cannot parse move")
+}
+
+func parseColumnTwo(c string) (shape, error) {
+	switch c {
+	case "X":
+		return rock, nil
+	case "Y":
+		return paper, nil
+	case "Z":
+		return scissors, nil
+	}
+
+	return rock, errors.New("Cannot parse move")
 }
 
 func readFile(filepath string) string {
