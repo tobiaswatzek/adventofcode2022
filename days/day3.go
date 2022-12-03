c5f3973c2ce938eac6779faba2707afd4fcbc4f5
Tobias Watzek <spitzfaust@gmail.com>
2022-12-03 11:09:44 +0100

    Add solution for day 3 part 2

--- days/day3.go
+++ days/day3.go
@@ -11,11 +11,16 @@ import (
 func day3(inputFilePath string) (string, string, error) {
 	input := util.ReadFile(inputFilePath)
 	rucksacks := strings.Split(input, "\n")
+
+	solutionOne := solveDay3PartOne(rucksacks[:len(rucksacks)-1])
+	solutionTwo := solveDay3PartTwo(rucksacks[:len(rucksacks)-1])
+
+	return fmt.Sprint(solutionOne), fmt.Sprint(solutionTwo), nil
+}
+
+func solveDay3PartOne(rucksacks []string) int {
 	priority := 0
 	for _, rucksack := range rucksacks {
-		if strings.TrimSpace(rucksack) == "" {
-			continue
-		}
 		middle := len(rucksack) / 2
 		compartmentOne := mapset.NewSet([]byte(rucksack[0:middle])...)
 		compartmentTwo := mapset.NewSet([]byte(rucksack[middle:])...)
@@ -25,7 +30,27 @@ func day3(inputFilePath string) (string, string, error) {
 		priority += itemPriority(inBothCompartments)
 	}
 
-	return fmt.Sprint(priority), "", nil
+	return priority
+}
+
+func solveDay3PartTwo(rucksacks []string) int {
+	badge := mapset.NewSet[byte]()
+	priority := 0
+	for i, rucksack := range rucksacks {
+		items := mapset.NewSet([]byte(rucksack)...)
+
+		if i%3 == 0 {
+			badge = items
+		} else {
+			badge = badge.Intersect(items)
+		}
+
+		if i%3 == 2 {
+			priority += itemPriority(badge.ToSlice()[0])
+		}
+	}
+
+	return priority
 }
 
 func itemPriority(item byte) int {
