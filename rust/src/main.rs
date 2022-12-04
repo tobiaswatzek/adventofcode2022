f7c12728e70dc33e2d2a953fcd37e622483ab6b4
Tobias Watzek <spitzfaust@gmail.com>
2022-12-04 13:06:35 +0100

    Add command line parsing for rust solution

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,15 +1,43 @@
-use std::fs;
+use std::{env, error, fs, path::{PathBuf}};
 
 fn main() {
-    let file_path = "../data/day1.txt";
 
-    let (part_one, part_two) = day1(file_path);
+    let args = parse_args().expect("arguments are expected");
+
+    let (part_one, part_two) = day1(&args.file_path());
 
     println!("Day 1:\n\tPart one: {part_one}\n\tPart two: {part_two}");
 }
 
+#[derive(Debug)]
+struct Arguments {
+    data_dir: PathBuf,
+    day: u8,
+}
 
-fn day1(input_path: &str) -> (String, String)  {
+impl Arguments {
+    fn file_path(&self) -> PathBuf {
+        self.data_dir.join(format!("day{}.txt", self.day))
+    }
+}
+
+fn parse_args() -> Result<Arguments, Box<dyn error::Error>> {
+    let args: Vec<String> = env::args().collect();
+
+    let day = match args[1].parse::<u8>()? {
+        d @ 1..=24 => d,
+        d => return Err(format!("day must be between 1 and 24 but is {d}").into()),
+    };
+
+    let data_dir = match args[2].trim() {
+        s if !s.is_empty() => PathBuf::from(s),
+        _ => return Err("data dir must be passed as argument".into()),
+    };
+
+    Ok(Arguments { data_dir, day })
+}
+
+fn day1(input_path: &PathBuf) -> (String, String) {
     let input = fs::read_to_string(input_path).expect("Should have been able to read the file");
 
     let mut calories_per_elf = input
