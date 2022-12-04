41b774bdf396d6aef2b37dc18511fbdac6c0148e
Tobias Watzek <spitzfaust@gmail.com>
2022-12-04 15:10:00 +0100

    Add solution for day 3 in rust

--- rust/src/days.rs
+++ rust/src/days.rs
@@ -1,10 +1,12 @@
 use std::{collections::HashMap, path::PathBuf};
 
 mod day1;
+mod day3;
 
 pub fn solve_day(day: &u8, input_path: &PathBuf) -> (String, String) {
-    let mut day_registry = HashMap::new();
+    let mut day_registry: HashMap<u8, fn (&PathBuf) -> (String, String)> = HashMap::new();
     day_registry.insert(1, day1::solve);
+    day_registry.insert(3, day3::solve);
 
     let solve = day_registry
         .get(day)
