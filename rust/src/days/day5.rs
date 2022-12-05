f85dca2d682b916e0563c8ca4463626f42fa935e
Tobias Watzek <spitzfaust@gmail.com>
2022-12-05 21:55:56 +0100

    Add parser for stacks of day 5

--- rust/src/days/day5.rs
+++ rust/src/days/day5.rs
@@ -1,23 +1,21 @@
 use std::{
-    collections::{HashMap, VecDeque},
+    collections::{VecDeque},
     fs,
     path::PathBuf,
 };
 
 pub fn solve(input_path: &PathBuf) -> (String, String) {
     let input = fs::read_to_string(input_path).expect("should have been able to read the file");
-
-    // let stacks = parse_keys(input);
     let instructions: Vec<CraneInstruction> = parse_instructions(&input);
 
-    let part_one = solve_part_one(&mut build_stacks(), &instructions);
-    let part_two = solve_part_two(&mut build_stacks(), &instructions);
+    let part_one = solve_part_one(&mut build_stacks(&input), &instructions);
+    let part_two = solve_part_two(&mut build_stacks(&input), &instructions);
 
     return (part_one, part_two);
 }
 
 fn solve_part_one(
-    stacks: &mut Vec<VecDeque<char>>,
+    stacks: &mut [VecDeque<char>; 9],
     instructions: &Vec<CraneInstruction>,
 ) -> String {
     for instruction in instructions {
@@ -33,7 +31,7 @@ fn solve_part_one(
 }
 
 fn solve_part_two(
-    stacks: &mut Vec<VecDeque<char>>,
+    stacks: &mut [VecDeque<char>; 9],
     instructions: &Vec<CraneInstruction>,
 ) -> String {
     for instruction in instructions {
@@ -53,18 +51,20 @@ fn solve_part_two(
     stacks.iter().map(|s| s.front()).filter_map(|s| s).collect()
 }
 
-fn build_stacks() -> Vec<VecDeque<char>> {
-    let mut stacks = Vec::new();
+fn build_stacks(input: &str) -> [VecDeque<char>; 9] {
+    let mut stacks: [VecDeque<char>; 9] = Default::default();
 
-    stacks.push(VecDeque::from(['G', 'B', 'D', 'C', 'P', 'R']));
-    stacks.push(VecDeque::from(['G', 'V', 'H']));
-    stacks.push(VecDeque::from(['M', 'P', 'J', 'D', 'Q', 'S', 'N']));
-    stacks.push(VecDeque::from(['M', 'N', 'C', 'D', 'G', 'L', 'S', 'P']));
-    stacks.push(VecDeque::from(['S', 'L', 'F', 'P', 'C', 'N', 'B', 'J']));
-    stacks.push(VecDeque::from(['S', 'T', 'G', 'V', 'Z', 'D', 'B', 'Q']));
-    stacks.push(VecDeque::from(['Q', 'T', 'F', 'H', 'M', 'Z', 'B']));
-    stacks.push(VecDeque::from(['F', 'B', 'D', 'M', 'C']));
-    stacks.push(VecDeque::from(['G', 'Q', 'C', 'F']));
+    for line in input.lines().take(8) {
+        line.chars()
+            .collect::<Vec<char>>()
+            .chunks(4)
+            .map(|chunk| chunk.iter().filter(|c| c.is_alphabetic()).nth(0))
+            .enumerate()
+            .for_each(|(i, item)| match item {
+                Some(c) => stacks[i].push_back(c.to_owned()),
+                None => (),
+            });
+    }
 
     stacks
 }
@@ -91,29 +91,4 @@ struct CraneInstruction {
     count: usize,
     from: usize,
     to: usize,
-}
-
-fn parse_keys(input: String) -> HashMap<u8, VecDeque<char>> {
-    input
-        .lines()
-        .find(|l| l.starts_with(" 1"))
-        .and_then(|l| {
-            let keys = l
-                .trim()
-                .split("   ")
-                .map(|s| s.parse::<u8>().expect("key must be a number"));
-
-            let mut map: HashMap<u8, VecDeque<char>> = HashMap::new();
-
-            for key in keys {
-                map.insert(key, VecDeque::new());
-            }
-
-            if map.is_empty() {
-                return None;
-            }
-
-            Some(map)
-        })
-        .expect("keys must be found")
 }
