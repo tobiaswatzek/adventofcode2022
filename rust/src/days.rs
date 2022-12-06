ecd4868b089d8cfa6087b7287ab04d558111800b
Tobias Watzek <spitzfaust@gmail.com>
2022-12-06 07:09:34 +0100

    Add solution for day 6

--- rust/src/days.rs
+++ rust/src/days.rs
@@ -3,12 +3,14 @@ use std::{collections::HashMap, path::PathBuf};
 mod day1;
 mod day3;
 mod day5;
+mod day6;
 
 pub fn solve_day(day: &u8, input_path: &PathBuf) -> (String, String) {
     let mut day_registry: HashMap<u8, fn (&PathBuf) -> (String, String)> = HashMap::new();
     day_registry.insert(1, day1::solve);
     day_registry.insert(3, day3::solve);
     day_registry.insert(5, day5::solve);
+    day_registry.insert(6, day6::solve);
 
     let solve = day_registry
         .get(day)
