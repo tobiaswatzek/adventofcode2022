671b83b7ad8fc05bda7300a4ff8f9bffe6fd1c3b
Tobias Watzek <spitzfaust@gmail.com>
2022-12-12 20:51:01 +0100

    Add solution for day 12 part 2

--- rust/src/days/day12.rs
+++ rust/src/days/day12.rs
@@ -2,26 +2,116 @@ use std::{
     collections::{HashMap, HashSet, VecDeque},
     fs,
     path::PathBuf,
+    time::Instant,
 };
 
 pub fn solve(input_path: &PathBuf) -> (String, String) {
     let input = fs::read_to_string(input_path).unwrap();
-    let height_map = parse_input(&input);
+    let grid = parse_point_value_grid(&input);
 
-    let test = bfs(&height_map);
 
-    return (test.to_string(), String::new());
+    let part_one = solve_part_one(&grid);
+    let before = Instant::now();
+    let part_two = solve_part_two(&grid);
+    println!("Elapsed: {:.2?}", before.elapsed());
+
+    return (part_one.to_string(), part_two.to_string());
 }
 
-fn bfs(height_map: &HeightMap) -> usize {
+fn solve_part_one(grid: &Vec<Vec<PointValue>>) -> usize {
+    let nodes = connect_nodes(&grid);
+    let start_end = grid
+        .iter()
+        .enumerate()
+        .flat_map(|(y, row)| {
+            row.iter()
+                .enumerate()
+                .filter_map(move |(x, PointValue(_, p))| match p {
+                    PointType::Start => Some((PointType::Start, Point { x, y })),
+                    PointType::End => Some((PointType::End, Point { x, y })),
+                    _ => None,
+                })
+        })
+        .collect::<Vec<_>>();
+
+    let start = start_end
+        .iter()
+        .find(|(t, _)| t == &PointType::Start)
+        .unwrap()
+        .1;
+    let end = start_end
+        .iter()
+        .find(|(t, _)| t == &PointType::End)
+        .unwrap()
+        .1;
+
+    let height_map = HeightMap {
+        start,
+        end,
+        nodes: &nodes,
+    };
+
+    bfs(&height_map).unwrap()
+}
+
+fn solve_part_two(grid: &Vec<Vec<PointValue>>) -> usize {
+    let nodes = connect_nodes(&grid);
+    let start_end = grid
+        .iter()
+        .enumerate()
+        .flat_map(|(y, row)| {
+            row.iter()
+                .enumerate()
+                .filter_map(move |(x, PointValue(v, t))| {
+                    if v == &('a' as u32) {
+                        return Some((PointType::Start, Point { x, y }));
+                    }
+
+                    if t == &PointType::End {
+                        return Some((PointType::End, Point { x, y }));
+                    }
+
+                    None
+                })
+        })
+        .collect::<Vec<_>>();
+
+    let starts = start_end.iter().filter_map(|(t, p)| match t {
+        PointType::Start => Some(p),
+        _ => None,
+    });
+
+    let end = start_end
+        .iter()
+        .find(|(t, _)| t == &PointType::End)
+        .unwrap()
+        .1;
+
+    starts
+        .filter_map(|&s| {
+            let hm = HeightMap {
+                start: s,
+                end,
+                nodes: &nodes,
+            };
+            bfs(&hm)
+        })
+        .min()
+        .unwrap()
+}
+
+fn bfs(height_map: &HeightMap) -> Option<usize> {
     let mut predecessors = HashMap::new();
     let mut visited = HashSet::new();
     let mut queue = VecDeque::new();
     queue.push_back(height_map.start);
     visited.insert(height_map.start);
 
+    let mut found_end = false;
+
     while let Some(point) = queue.pop_front() {
         if point == height_map.end {
+            found_end = true;
             break;
         }
 
@@ -34,6 +124,10 @@ fn bfs(height_map: &HeightMap) -> usize {
         }
     }
 
+    if !found_end {
+        return None;
+    }
+
     let mut key = height_map.end;
     let mut path = Vec::new();
     path.push(key);
@@ -42,7 +136,57 @@ fn bfs(height_map: &HeightMap) -> usize {
         key = p;
     }
 
-    path.len() - 1
+    Some(path.len() - 1)
+}
+
+fn parse_point_value_grid(input: &str) -> Vec<Vec<PointValue>> {
+    input
+        .lines()
+        .map(|l| {
+            l.chars()
+                .map(|c| match c {
+                    'S' => PointValue('a' as u32, PointType::Start),
+                    'E' => PointValue('z' as u32, PointType::End),
+                    n => PointValue(n as u32, PointType::None),
+                })
+                .collect::<Vec<PointValue>>()
+        })
+        .collect::<Vec<Vec<PointValue>>>()
+}
+
+fn connect_nodes(grid: &Vec<Vec<PointValue>>) -> HashMap<Point, Vec<Point>> {
+    let mut nodes: HashMap<Point, Vec<Point>> = HashMap::with_capacity(grid.len() * grid[0].len());
+
+    for (y, row) in grid.iter().enumerate() {
+        for (x, _) in row.iter().enumerate() {
+            let point = Point { x, y };
+            nodes.insert(point, build_nodes(&point, &grid));
+        }
+    }
+
+    nodes
+}
+
+fn build_nodes(current: &Point, point_value_grid: &Vec<Vec<PointValue>>) -> Vec<Point> {
+    let PointValue(current_weight, _) = point_value_grid[current.y][current.x];
+
+    current
+        .neighbors()
+        .iter()
+        .filter_map(|&neighbor| match neighbor {
+            Some(p) => point_value_grid
+                .get(p.y)
+                .and_then(|row| row.get(p.x))
+                .and_then(|&PointValue(w, _)| {
+                    if w <= current_weight + 1 {
+                        return Some(p);
+                    }
+
+                    None
+                }),
+            None => None,
+        })
+        .collect()
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
@@ -92,10 +236,10 @@ impl Point {
 }
 
 #[derive(Debug, Clone)]
-struct HeightMap {
+struct HeightMap<'a> {
     start: Point,
     end: Point,
-    nodes: HashMap<Point, Vec<Point>>,
+    nodes: &'a HashMap<Point, Vec<Point>>,
 }
 
 #[derive(Debug, Clone, PartialEq)]
@@ -107,63 +251,3 @@ enum PointType {
 
 #[derive(Debug, Clone, PartialEq)]
 struct PointValue(u32, PointType);
-
-fn parse_input(input: &str) -> HeightMap {
-    let point_value_grid = input
-        .lines()
-        .map(|l| {
-            l.chars()
-                .map(|c| match c {
-                    'S' => PointValue('a' as u32, PointType::Start),
-                    'E' => PointValue('z' as u32, PointType::End),
-                    n => PointValue(n as u32, PointType::None),
-                })
-                .collect::<Vec<PointValue>>()
-        })
-        .collect::<Vec<Vec<PointValue>>>();
-
-    let mut nodes: HashMap<Point, Vec<Point>> =
-        HashMap::with_capacity(point_value_grid.len() * point_value_grid[0].len());
-    let mut start: Option<Point> = None;
-    let mut end: Option<Point> = None;
-
-    for (y, row) in point_value_grid.iter().enumerate() {
-        for (x, cell) in row.iter().enumerate() {
-            let point = Point { x, y };
-            match cell.1 {
-                PointType::Start => start = Some(point),
-                PointType::End => end = Some(point),
-                _ => (),
-            };
-            nodes.insert(point, build_nodes(&point, &point_value_grid));
-        }
-    }
-
-    HeightMap {
-        start: start.unwrap(),
-        end: end.unwrap(),
-        nodes,
-    }
-}
-
-fn build_nodes(current: &Point, point_value_grid: &Vec<Vec<PointValue>>) -> Vec<Point> {
-    let PointValue(current_weight, _) = point_value_grid[current.y][current.x];
-
-    current
-        .neighbors()
-        .iter()
-        .filter_map(|&neighbor| match neighbor {
-            Some(p) => point_value_grid
-                .get(p.y)
-                .and_then(|row| row.get(p.x))
-                .and_then(|&PointValue(w, _)| {
-                    if w <= current_weight + 1 {
-                        return Some(p);
-                    }
-
-                    None
-                }),
-            None => None,
-        })
-        .collect()
-}
