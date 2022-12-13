e86eab5565f92bac1c7d459f03bd04c61dc0973d
Tobias Watzek <spitzfaust@gmail.com>
2022-12-13 20:11:49 +0100

    Start solution for day 13 part 1

--- rust/src/days.rs
+++ rust/src/days.rs
@@ -10,6 +10,7 @@ mod day7;
 mod day8;
 mod day9;
 mod day12;
+mod day13;
 
 pub fn solve_day(day: &u8, input_path: &PathBuf) -> (String, String) {
     let mut day_registry: HashMap<u8, fn(&PathBuf) -> (String, String)> = HashMap::new();
@@ -23,6 +24,7 @@ pub fn solve_day(day: &u8, input_path: &PathBuf) -> (String, String) {
     day_registry.insert(10, day10::solve);
     day_registry.insert(11, day11::solve);
     day_registry.insert(12, day12::solve);
+    day_registry.insert(13, day13::solve);
 
     let solve = day_registry
         .get(day)
